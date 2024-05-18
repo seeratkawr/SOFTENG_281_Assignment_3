339b5b1e0119158e45762e200fb591419705802e
Seerat Mann <sman833@aucklanduni.ac.nz>
2024-05-18 20:31:23 +1200

    implemented loadMap() to split countries.csv. Started implementing showInfoCountry.

--- src/main/java/nz/ac/auckland/se281/MapEngine.java
+++ src/main/java/nz/ac/auckland/se281/MapEngine.java
@@ -1,9 +1,13 @@
 package nz.ac.auckland.se281;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /** This class is the main entry point. */
 public class MapEngine {
+  List<String> country = new ArrayList<>();
+  List<String> continent = new ArrayList<>();
+  List<String> fees = new ArrayList<>();
 
   public MapEngine() {
     // add other code here if you want
@@ -14,12 +18,28 @@ public class MapEngine {
   private void loadMap() {
     List<String> countries = Utils.readCountries();
     List<String> adjacencies = Utils.readAdjacencies();
-    // add code here to create your data structures
+
+    for (String c : countries) {
+      String[] parts = c.split(",");
+      country.add(parts[0]);
+      continent.add(parts[1]);
+      fees.add(parts[2]);
+    }
   }
 
   /** this method is invoked when the user run the command info-country. */
   public void showInfoCountry() {
-    // add code here
+    MessageCli.INSERT_COUNTRY.printMessage();
+
+    String input = Utils.scanner.nextLine();
+
+    while (!country.contains(input)) {
+      MessageCli.INVALID_COUNTRY.printMessage(input);
+      input = Utils.scanner.nextLine();
+    }
+
+    int index = country.indexOf(input);
+    MessageCli.COUNTRY_INFO.printMessage(country.get(index), continent.get(index), fees.get(index));
   }
 
   /** this method is invoked when the user run the command route. */
